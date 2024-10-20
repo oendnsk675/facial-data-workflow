95478805f5c3e73d846b5a1eeb366d89a3b051b6
osi <sayidinaahmadalqososyi@gmail.com>
2024-10-20 11:44:17 +0800

    update parameter

--- dags/airflow_dag.py
+++ dags/airflow_dag.py
@@ -46,25 +46,30 @@ def load_frames_to_folder(output_dir):
     return output_dir
 
 with DAG(dag_id='video_etl', default_args=default_args, schedule_interval='@daily') as dag:
+    filename = "happy.mp4"
+    data_type = "happy"
+    data_source = f"./data/raw/{filename}"
+    data_dest = f"./data/transformed/{data_type}"
+    
     # Step 1: Extract video
     extract = PythonOperator(
         task_id='extract_video',
         python_callable=extract_video,
-        op_kwargs={'video_path': './data/raw/angry.mp4'},
+        op_kwargs={'video_path': data_source},
     )
 
     # Step 2: Transform video to frames
     transform = PythonOperator(
         task_id='transform_video_to_frames',
         python_callable=transform_video_to_frames,
-        op_kwargs={'video_path': './data/raw/angry.mp4', 'output_dir': './data/transformed'},
+        op_kwargs={'video_path': data_source, 'output_dir': data_dest},
     )
 
     # Step 3: Load the frames
     load = PythonOperator(
         task_id='load_frames',
         python_callable=load_frames_to_folder,
-        op_kwargs={'output_dir': './data/transformed'},
+        op_kwargs={'output_dir': data_dest},
     )
 
     extract >> transform >> load
